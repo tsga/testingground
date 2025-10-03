58706f6e12ad3efb73c4749fb73ca32a35c00743
Tseganeh Z. Gichamo <zacctsega@yahoo.com>
2025-10-03 10:38:03 -0400

    Update 135_ncdiff.py

--- 135_ncdiff.py
+++ 135_ncdiff.py
@@ -233,6 +233,101 @@ def plot_3inc(file1in, file2in, file3in, varin, output_file,
         plt.show()
 
 
+def plot_3fc_diff(file1in, file2in, file3in, file1o, file2o, file3o, varin, output_file,
+                           title1="fhr003", title2="fhr006", title3="fhr009") -> None:
+    """
+    
+    """
+
+    # Load datasets
+    file1 = Dataset(file1in)
+    file2 = Dataset(file2in)
+    file3 = Dataset(file3in)
+
+    # Extract soilt1 for time=0 and coordinates
+    soilti_file1 = file1[varin][:] #.isel(time=0)
+    soilti_file2 = file2[varin][:] #.isel(time=0)
+    soilti_file3 = file3[varin][:] #.isel(time=0)
+
+    # Use 2D lat/lon grids for axes
+    lat = file1['latitude'][:]      # shape: (grid_yt, grid_xt)
+    lon = file1['longitude'][:]      # shape: (grid_yt, grid_xt)
+    
+    file1.close()
+    file2.close()
+    file3.close()
+
+    file1 = Dataset(file1o)
+    file2 = Dataset(file2o)
+    file3 = Dataset(file3o)
+
+    # Extract soilt1 for time=0 and coordinates
+    soilto_file1 = file1[varin][:] #.isel(time=0)
+    soilto_file2 = file2[varin][:] #.isel(time=0)
+    soilto_file3 = file3[varin][:] #.isel(time=0)
+                             
+    file1.close()
+    file2.close()
+    file3.close()
+
+    soilt1_file1 = soilto_file1 - soilti_file1
+    soilt1_file2 = soilto_file2 - soilti_file2
+    soilt1_file3 = soilto_file3 - soilti_file3
+
+    # Mask invalid values
+    file1_masked = np.ma.masked_invalid(soilt1_file1)
+    file2_masked = np.ma.masked_invalid(soilt1_file2)
+    file3_masked = np.ma.masked_invalid(soilt1_file3)
+
+    std_1 = np.nanstd(file1_masked)
+    vmin_1 = -3 * std_1
+    vmax_1 = 3 * std_1
+
+    std_2 = np.nanstd(file2_masked)
+    vmin_2 = -3 * std_2
+    vmax_2 = 3 * std_2
+
+    std_3 = np.nanstd(file3_masked)
+    vmin_3 = -3 * std_3
+    vmax_3 = 3 * std_3
+
+    # Plotting
+    fig, axes = plt.subplots(3, 1, figsize=(10, 15), subplot_kw={'projection': ccrs.PlateCarree()}, constrained_layout=True)
+
+    # Subplot 1: file2 - file1
+    pc0 = axes[0].pcolormesh(lon, lat, file1_masked,cmap='coolwarm', shading='auto', vmin=vmin_1, vmax=vmax_1, transform=ccrs.PlateCarree())
+    axes[0].set_title(f'{title1}')
+    axes[0].coastlines()
+    #axes[0].add_feature(cfeature.BORDERS, linestyle=':')
+    axes[0].set_xlabel('Longitude')
+    axes[0].set_ylabel('Latitude')
+    fig.colorbar(pc0, ax=axes[0], orientation='vertical', fraction=0.046, pad=0.04)
+    
+    pc1 = axes[1].pcolormesh(lon, lat, file2_masked,cmap='coolwarm', shading='auto', vmin=vmin_2, vmax=vmax_2, transform=ccrs.PlateCarree())
+    axes[1].set_title(f'{title2}')
+    axes[1].coastlines()
+    #axes[0].add_feature(cfeature.BORDERS, linestyle=':')
+    axes[1].set_xlabel('Longitude')
+    axes[1].set_ylabel('Latitude')
+    fig.colorbar(pc1, ax=axes[1], orientation='vertical', fraction=0.046, pad=0.04)
+
+    # Subplot 2: file3 - file1
+    pc2 = axes[2].pcolormesh(lon, lat, file3_masked, cmap='coolwarm', shading='auto', vmin=vmin_3, vmax=vmax_3, transform=ccrs.PlateCarree())
+    axes[2].set_title(f'{title3}')
+    axes[2].coastlines()
+    #axes[1].add_feature(cfeature.BORDERS, linestyle=':')
+    axes[2].set_xlabel('Longitude')
+    axes[2].set_ylabel('Latitude')
+    fig.colorbar(pc2, ax=axes[2], orientation='vertical', fraction=0.046, pad=0.04)
+
+    plt.show()
+
+    if output_file:
+        plt.savefig(output_file, bbox_inches='tight')
+    else:
+        plt.show()
+
+
 
 
 workdir="/lfs/h2/emc/da/noscrub/tseganeh.gichamo/PLT/"
@@ -242,6 +337,25 @@ dadir="/lfs/h2/emc/ptmp/tseganeh.gichamo/soilda/"
 noldir="/lfs/h2/emc/ptmp/tseganeh.gichamo/noliau/"
 
 os.chdir(workdir)
+
+
+file1i=contdir+"enkfgdas.20220501/06/ensstat/model/atmos/history/enkfgdas.t06z.sfcf003.ensmean.nc"
+file2i=contdir+"enkfgdas.20220501/06/ensstat/model/atmos/history//enkfgdas.t06z.sfcf006.ensmean.nc"
+file3i=contdir+"enkfgdas.20220501/06/ensstat/model/atmos/history/enkfgdas.t06z.sfcf009.ensmean.nc"
+
+file1o=dadir+"enkfgdas.20220501/06/ensstat/model/atmos/history/enkfgdas.t06z.sfcf003.ensmean.nc"
+file2o=dadir+"enkfgdas.20220501/06/ensstat/model/atmos/history//enkfgdas.t06z.sfcf006.ensmean.nc"
+file3o=dadir+"enkfgdas.20220501/06/ensstat/model/atmos/history/enkfgdas.t06z.sfcf009.ensmean.nc"
+
+plot_3fc_diff(file1i, file2i, file3i, file1o, file2o, file3o, varin, "stc1_da_cont.png",
+                            title1="Diff (DA-Control) enkfgdas.20220501/06/ensstat/sfcf003.ensmean soilt1 fhr003", title2="Diff stc1 fhr006", title3="Diff stc1 fhr009")
+
+file1o=noldir+"enkfgdas.20220501/06/ensstat/model/atmos/history/enkfgdas.t06z.sfcf003.ensmean.nc"
+file2o=noldir+"enkfgdas.20220501/06/ensstat/model/atmos/history//enkfgdas.t06z.sfcf006.ensmean.nc"
+file3o=noldir+"enkfgdas.20220501/06/ensstat/model/atmos/history/enkfgdas.t06z.sfcf009.ensmean.nc"
+plot_3fc_diff(file1i, file2i, file3i, file1o, file2o, file3o, varin, "stc1_nol_cont.png",
+                            title1="Diff (DAnoliau-Control) enkfgdas.20220501/06/ensstat/sfcf003.ensmean soilt1 fhr003", title2="Diff stc1 fhr006", title3="Diff stc1 fhr009")
+exit(0)
 
 file1=dadir+"enkfgdas.20220501/06/ensstat/analysis/atmos/enkfgdas.t06z.sfci003.nc"
 file2=dadir+"enkfgdas.20220501/06/ensstat/analysis/atmos/enkfgdas.t06z.sfci006.nc"
